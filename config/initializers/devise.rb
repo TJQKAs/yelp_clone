38397ff19a731e60d362c655ea6e0f2e9d0e127d
TJQKAs <a26052013@gmail.com>
2015-10-14 10:47:27 +0100

    version 2.0 update

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -13,7 +13,7 @@ Devise.setup do |config|
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
   config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'
-
+  config.omniauth :facebook, "ai5834387@gmail.com", "aa100200300aa"
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
 
