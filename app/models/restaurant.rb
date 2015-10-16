a58675be1d30cd26661a95e8c1c2f85d34a4b0e3
TJQKAs <a26052013@gmail.com>
2015-10-16 15:18:31 +0100

    we add picture to the restaurant review

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,4 +1,6 @@
 class Restaurant < ActiveRecord::Base
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, default_url: ->(attachment) { ActionController::Base.helpers.asset_path('missing.jpg')}
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   has_many :reviews, dependent: :destroy
   validates :name, length: {minimum: 3}, uniqueness: true
 
